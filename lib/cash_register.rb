397733dfd1bd291251651e662e7341e164de55ac
Daysha Jones <daysha.vu@icloud.com>
2021-03-17 18:33:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,14 @@
 class CashRegister
-  
-  attr_accessor :total
-  attr_reader :discount
-  
-  def initialize(discount = 0)
-    @total = 0
-    @discount = discount
-  end
-  
-  def add_item(title, price, quanity = 1)
+attr_reader :discount, :items
+attr_accessor :total
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(title, price, quanity = 1)
         quanity.times do
             @items << title
         end
@@ -16,17 +16,16 @@ class CashRegister
         @total += price * quanity
     end
 
-  def apply_discount
-      @total -= @total * @discount / 100
-      if @discount == 0
-          "There is no discount to apply."
-      else
-          "After the discount, the total comes to $#{@total}."
-      end
-  end
+    def apply_discount
+        @total -= @total * @discount / 100
+        if @discount == 0
+            "There is no discount to apply."
+        else
+            "After the discount, the total comes to $#{@total}."
+        end
+    end
 
-  def void_last_transaction
-      @total = @last_total
-  end
-  
+    def void_last_transaction
+        @total = @last_total
+    end
 end
