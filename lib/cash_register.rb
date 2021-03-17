0fc9c22c3a580e048bad002caeeb9e06d4e8e782
Daysha Jones <daysha.vu@icloud.com>
2021-03-17 18:32:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,9 +8,26 @@ class CashRegister
     @discount = discount
   end
   
-  def add_item
-    
-  end
+  def add_item(title, price, quanity = 1)
+        quanity.times do
+            @items << title
+        end
+        @last_total = @total
+        @total += price * quanity
+    end
+
+    def apply_discount
+        @total -= @total * @discount / 100
+        if @discount == 0
+            "There is no discount to apply."
+        else
+            "After the discount, the total comes to $#{@total}."
+        end
+    end
+
+    def void_last_transaction
+        @total = @last_total
+    end
   
   
   
